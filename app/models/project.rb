dd254f49606b0b683ae7538562b43ea7a9667697
tvcou <tvcou.zyx77@gmail.com>
2015-05-17 05:10:37 +0000

    "err message"

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ActiveRecord::Base
-  validates :title, presence: true
+#  validates :title, presence: true     # デフォルトメッセージ
+  validates :title, presence: {message: "入力してください"}
 end
