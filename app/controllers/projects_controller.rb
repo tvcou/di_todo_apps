23dc1052d83b6e46d4d5e15ca74766827ba2eb1c
tvcou <tvcou.zyx77@gmail.com>
2015-05-17 04:31:01 +0000

    "make show"

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,9 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
+
+  def show
+    @project = Project.find(params[:id])
+    #URLで渡ってきた数字はparamsで取ってこれる。シンボルはroutesに書かれているid。
+  end
 end
