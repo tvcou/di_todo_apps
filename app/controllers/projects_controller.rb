2aa16743257b265905737d85dd9f229a6f590159
tvcou <tvcou.zyx77@gmail.com>
2015-05-17 04:58:16 +0000

    "make new"

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,6 +5,28 @@ class ProjectsController < ApplicationController
 
   def show
     @project = Project.find(params[:id])
-    #URLで渡ってきた数字はparamsで取ってこれる。シンボルはroutesに書かれているid。
+      #URLで渡ってきた数字はparamsで取ってこれる。シンボルはroutesに書かれているid。
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    @project.save
+    redirect_to projects_path
+  end
+
+  private
+    def project_params
+      params.require(:project).permit(:title)
+    end
+    #project_paramsについてはprivate関数で定義するのが代表的な手法
+    #params[:project].permit[:title] projectで渡ってきた中の物のうち、
+    #タイトルだけを引っ張ってきてねという意味。
+    #セキュリティ上、フォームから渡ってきたものについて
+    #プロジェクトとタイトルだけ必要という時は
+    #こういうふうにフィルタリングをかけるのが最近のやり方。
+
 end
