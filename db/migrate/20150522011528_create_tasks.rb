8d1e809eb508f6032ce7345a4cbab985a14164d1
tvcou <tvcou.zyx77@gmail.com>
2015-05-22 01:17:43 +0000

    "vi db/migrate/20150522011528_create_tasks.rb, rake db:migrate"

--- db/migrate/20150522011528_create_tasks.rb
+++ db/migrate/20150522011528_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
       t.string :title
-      t.boolean :done
+      t.boolean :done, default: false
       t.references :project, index: true, foreign_key: true
 
       t.timestamps null: false
