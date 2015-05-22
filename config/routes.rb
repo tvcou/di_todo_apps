b7a280e0354a42a0abdb190d5a5325434157a6d7
tvcou <tvcou.zyx77@gmail.com>
2015-05-22 02:24:24 +0000

    "mod config/routes.rb"

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  resources :projects do
+    resources :tasks, only: [:create, :destroy]
+  end
   root 'projects#index'
-  resources :projects
-  resources :tasks, only: [:create, :destroy]
 end
